90b20cb457fd3903a9001af982a82dd565515c79
alex <alex@mystenlabs.com>
2024-03-19 07:28:39 +0200

    Initial commit

--- scripts/publishing/publish.sh
+++
@@ -1,6 +0,0 @@
-echo "You are currently using the following env:"
-sui client active-env
-echo "Make sure it is correct or press ctrl+c to cancel within 3 seconds."
-sleep 3
-echo "Proceeding with publishing"
-sui client publish --json --gas-budget 1000000000 ../../ > publish.json
